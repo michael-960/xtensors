9aaa440a0c4b8f8020f880c2e37eee6daaa752a2
michael-960 <mike.wang96029@gapp.nthu.edu.tw>
2022-11-21 11:14:26 +0800

    docs

---
+++ docs/source/conf.py
@@ -0,0 +1,42 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'xtensors'
+copyright = '2022, Michael Wang'
+author = 'Michael Wang'
+release = '0.1.8'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc'
+]
+
+autodoc_member_order = 'bysource'
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'en'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+
+html_static_path = ['_static']
+
+
+def skip(app, what, name, obj, would_skip, options):
+    if name == "__init__":
+        return False
+    return would_skip
+
+def setup(app):
+    app.connect("autodoc-skip-member", skip)
---
+++ docs/source/index.rst
@@ -0,0 +1,21 @@
+.. xtensors documentation master file, created by
+   sphinx-quickstart on Mon Nov 21 11:10:52 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to xtensors's documentation!
+====================================
+
+.. toctree::
+   :maxdepth: 2
+
+   api 
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
